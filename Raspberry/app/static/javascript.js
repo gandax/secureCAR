38747eaa515c1450006aaeedd30617652a0780b0
MathiasFaro <faro@etud.insa-toulouse.fr>
2017-11-30 08:57:21 +0100

    [FIX] Change values for angles of direction
    Signed-off-by: MathiasFaro <faro@etud.insa-toulouse.fr>

--- Raspberry/app/static/javascript.js
+++ Raspberry/app/static/javascript.js
@@ -9,10 +9,10 @@ carData.controller('carController', ['$scope', '$http',
 	function($scope, $http){
 
 		// To change
-		const anglemax = 58;
+		const anglemax = 20;
 
 		var motorCommand = 0;
-		var directionCommand = 34;
+		var directionCommand = 10;
 
 		var keyup = 38;
 		var keydown = 40;
